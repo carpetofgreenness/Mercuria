7e28d45b11955e5e5758bf1d2e9ba68cb3395fe1
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-02-28 14:00:53 -0500

    create boat model and resources

---
+++ db/migrate/20170228185747_create_boats.rb
@@ -0,0 +1,12 @@
+class CreateBoats < ActiveRecord::Migration[5.0]
+  def change
+    create_table :boats do |t|
+      t.string :name
+      t.string :location
+      t.string :user_id
+      t.integer :container_num
+
+      t.timestamps
+    end
+  end
+end
