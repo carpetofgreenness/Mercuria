7fe4ce4a2dc7afd60391b8e448b660da61e06b2c
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-03-03 13:58:20 -0500

    used angular to include proper decimals and commas as user inputs job.cost. Changed migration so job>cost is a decimal. now working on getting the number submitted to actually bind to the job created

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,7 @@ class Job < ApplicationRecord
 	belongs_to :user
 	has_many :boat_jobs
 	has_many :boats, through: :boat_jobs
+
 	validates :origin, inclusion: {in: Locations::LOCATIONS}
 	validates :destination, inclusion: {in: Locations::LOCATIONS}
 
