ddbae3a47d921f05fbb78b2a956d7256557994e6
Jordan <Jordforb@gmail.com>
2017-03-03 14:42:33 -0500

    formatted job card

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 		@boats = @user.boats
 		@jobs = @user.jobs
+		@boat_jobs = BoatJob.all
 		@boat_job = BoatJob.new
 	end
 
