1c6a9a73b901c2281ea1e20d9c5c2e47b01423f2
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-03-06 12:40:32 -0500

    dropdown in _boat_card that assigns a job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job < ApplicationRecord
 	belongs_to :user
-	has_many :boat_jobs
+	has_many :boat_jobs, dependent: :destroy
 	has_many :boats, through: :boat_jobs
 
 	validates :origin, inclusion: {in: Locations::LOCATIONS}
